2008622fdf0347b288a04745d717d90fb263389c
Andrei Tan <drdrei@pm.me>
2024-05-28 20:58:29 +0000

    max request size

--- api/server.py
+++ api/server.py
@@ -1,9 +1,10 @@
 from .match_perms import search_permission
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-import json
+from typing_extensions import Annotated
+from typing import Union
 import uvicorn
 
 app = FastAPI()
@@ -30,7 +31,7 @@ def serve_spa():
 
 
 @app.get('/api/perm/search')
-async def search_perm(perm: str):
+async def search_perm(perm: Annotated[Union[str, None], Query(max_length=256)]):
     perms = search_permission(perm)
 
     return perms
